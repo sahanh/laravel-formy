da266cb9a23e35166d2bb58dd124823c27b497e9
Sahan H <sahan@sahanz.com>
2014-06-25 13:18:19 +0530

    refactored implemented session error populate

--- src/SH/LaravelFormy/LaravelValidator.php
+++ src/SH/LaravelFormy/LaravelValidator.php
@@ -4,8 +4,11 @@ namespace SH\LaravelFormy;
 use SH\Formy\ValidationInterface;
 use SH\Formy\Form;
 use Validator;
+use Session;
+use Illuminate\Support\MessageBag;
+use Illuminate\Support\Contracts\MessageProviderInterface;
 
-class LaravelValidator implements ValidationInterface
+class LaravelValidator implements ValidationInterface, MessageProviderInterface
 {
     protected $data;
 
@@ -22,6 +25,18 @@ class LaravelValidator implements ValidationInterface
     public function setForm(Form $form)
     {
         $this->form = $form;
+        $this->populateErrorsFromSession();
+    }
+
+    public function populateErrorsFromSession()
+    {
+        $errors = Session::get('errors');
+        
+        if (!$errors)
+            return false;
+
+        $this->messages = $errors->toArray();
+        $this->prepareForm();
     }
 
     public function setRules($rules)
@@ -38,7 +53,14 @@ class LaravelValidator implements ValidationInterface
         foreach ($this->validator->messages()->toArray() as $key => $errors)
             array_set($this->messages, $key, $errors);
 
+        $this->prepareForm();
+
         return $this->validator->passes();
+    }
+
+    public function getMessageBag()
+    {
+        return new MessageBag($this->messages);
     }
 
     public function getErrors()
@@ -53,13 +75,36 @@ class LaravelValidator implements ValidationInterface
 
     protected function getAttributeNames()
     {
-        foreach ($this->form->getFieldsets() as $fieldset_name => $fieldset) {
+        if (empty($this->attribute_names)) {
+            foreach ($this->form->getFieldsets() as $fieldset_name => $fieldset) {
 
-            foreach ($fieldset->getElements() as $element_name => $element)
-                array_set($this->attribute_names, "{$fieldset_name}.{$element_name}", $element->getLabel());
+                foreach ($fieldset->getElements() as $element_name => $element)
+                    array_set($this->attribute_names, "{$fieldset_name}.{$element_name}", $element->getLabel());
 
+            }
         }
 
         return array_dot($this->attribute_names);
+    }
+
+    protected function prepareForm()
+    {
+        foreach ($this->form->getFieldsets() as $fieldset_name => $fieldset) {
+            
+            $messages = $this->getFieldsetErrors($fieldset_name);
+
+            $this->prepareFormFieldset($fieldset, $messages);
+
+        }
+    }
+
+    protected function prepareFormFieldset($fieldset, $messages)
+    {
+        //foreach element in the fieldset
+        foreach ($fieldset->getElements() as $element_name => $element) {
+            //set error messages
+            if ($element_errors = array_get($messages, $element_name))
+                $element->setMeta('errors', $element_errors);
+        }
     }
 }
